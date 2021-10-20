ea00a71e0fd088e03ce853fcc4495527c5cbfe1f
giuxtaposition <yg97.cs@gmail.com>
2021-10-20 12:32:06 +0200

    made class Item more secure

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -1,116 +1,136 @@
 export class Item {
-  name: string;
-  sellIn: number;
-  quality: number;
+    private _name: string
+    private _sellIn: number
+    private _quality: number
 
-  constructor(name, sellIn, quality) {
-    this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
-  }
+    constructor(name, sellIn, quality) {
+        this._name = name
+        this._sellIn = sellIn
+        this._quality = quality
+    }
+
+    get name() {
+        return this._name
+    }
+
+    get sellIn() {
+        return this._sellIn
+    }
+
+    get quality() {
+        return this._quality
+    }
+
+    set sellIn(sellIn) {
+        this._sellIn = sellIn
+    }
+
+    set quality(quality) {
+        this._quality = quality
+    }
 }
 
 export class GildedRose {
-  items: Array<Item>;
+    items: Array<Item>
 
-  constructor(items = [] as Array<Item>) {
-    this.items = items;
-  }
-
-  updateQuality() {
-    this.items.forEach((item) => {
-      this.updateQualityBeforeSellIn(item);
-
-      this.decreaseSellIn(item);
-
-      if (this.hasSellInDatePassed(item)) {
-        this.updateQualityAfterSellIn(item);
-      }
-    });
-
-    return this.items;
-  }
-
-  updateQualityAfterSellIn(item) {
-    if (this.isAgedBrie(item)) {
-      this.increaseQuality(item, 1);
+    constructor(items = [] as Array<Item>) {
+        this.items = items
     }
 
-    if (this.isBackstagePasses(item)) {
-      this.decreaseQuality(item, item.quality);
+    updateQuality() {
+        this.items.forEach(item => {
+            this.updateQualityBeforeSellIn(item)
+
+            this.decreaseSellIn(item)
+
+            if (this.hasSellInDatePassed(item)) {
+                this.updateQualityAfterSellIn(item)
+            }
+        })
+
+        return this.items
     }
 
-    if (!this.isSpecialItem(item)) {
-      this.decreaseQuality(item, 1);
+    updateQualityAfterSellIn(item) {
+        if (this.isAgedBrie(item)) {
+            this.increaseQuality(item, 1)
+        }
+
+        if (this.isBackstagePasses(item)) {
+            this.decreaseQuality(item, item.quality)
+        }
+
+        if (!this.isSpecialItem(item)) {
+            this.decreaseQuality(item, 1)
+        }
     }
-  }
 
-  updateQualityBeforeSellIn(item) {
-    if (!this.isSpecialItem(item)) {
-      this.decreaseQuality(item, 1);
-    } else {
-      this.increaseQuality(item, 1);
+    updateQualityBeforeSellIn(item) {
+        if (!this.isSpecialItem(item)) {
+            this.decreaseQuality(item, 1)
+        } else {
+            this.increaseQuality(item, 1)
 
-      if (this.isBackstagePasses(item)) {
-        this.updateBackstagePassesQuality(item);
-      }
+            if (this.isBackstagePasses(item)) {
+                this.updateBackstagePassesQuality(item)
+            }
+        }
     }
-  }
 
-  updateBackstagePassesQuality(item) {
-    if (item.sellIn < 11 && item.quality < 50) {
-      this.increaseQuality(item, 1);
+    updateBackstagePassesQuality(item) {
+        if (item.sellIn < 11 && item.quality < 50) {
+            this.increaseQuality(item, 1)
+        }
+        if (item.sellIn < 6 && item.quality < 50) {
+            this.increaseQuality(item, 1)
+        }
     }
-    if (item.sellIn < 6 && item.quality < 50) {
-      this.increaseQuality(item, 1);
+
+    decreaseQuality(item, number) {
+        if (item.quality > 0) {
+            item.quality = this.isConjured(item)
+                ? item.quality - number - 1
+                : item.quality - number
+        }
     }
-  }
 
-  decreaseQuality(item, number) {
-    if (item.quality > 0) {
-      item.quality = this.isConjured(item)
-        ? item.quality - number - 1
-        : item.quality - number;
+    increaseQuality(item, number) {
+        if (item.quality < 50) {
+            item.quality = item.quality + number
+        }
     }
-  }
 
-  increaseQuality(item, number) {
-    if (item.quality < 50) {
-      item.quality = item.quality + number;
+    decreaseSellIn(item) {
+        if (!this.isSulfuras(item)) {
+            item.sellIn = item.sellIn - 1
+        }
     }
-  }
 
-  decreaseSellIn(item) {
-    if (!this.isSulfuras(item)) {
-      item.sellIn = item.sellIn - 1;
+    isSpecialItem(item) {
+        return (
+            this.isAgedBrie(item) ||
+            this.isBackstagePasses(item) ||
+            this.isSulfuras(item)
+        )
     }
-  }
 
-  isSpecialItem(item) {
-    return (
-      this.isAgedBrie(item) ||
-      this.isBackstagePasses(item) ||
-      this.isSulfuras(item)
-    );
-  }
+    isConjured(item) {
+        return item.name.includes('Conjured')
+    }
 
-  isConjured(item) {
-    return item.name.includes("Conjured");
-  }
+    isAgedBrie(item) {
+        return item.name === 'Aged Brie'
+    }
 
-  isAgedBrie(item) {
-    return item.name === "Aged Brie";
-  }
+    isBackstagePasses(item) {
+        return item.name === 'Backstage passes to a TAFKAL80ETC concert'
+    }
 
-  isBackstagePasses(item) {
-    return item.name === "Backstage passes to a TAFKAL80ETC concert";
-  }
+    isSulfuras(item) {
+        return item.name === 'Sulfuras, Hand of Ragnaros'
+    }
 
-  isSulfuras(item) {
-    return item.name === "Sulfuras, Hand of Ragnaros";
-  }
-
-  hasSellInDatePassed(item) {
-    return item.sellIn < 0;
-  }
+    hasSellInDatePassed(item) {
+        return item.sellIn < 0
+    }
 }
